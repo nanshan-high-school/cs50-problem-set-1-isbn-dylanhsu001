2bed3fe884c931b9b8747459a9413ed13b797953
dylanhsu001 <57486930+dylanhsu001@users.noreply.github.com>
2019-12-01 21:31:26 +0800

    第一版

---
+++ ISBN問題.cpp
@@ -0,0 +1,32 @@
+int main() {
+    
+    long long ISBN;
+    long long digitalISBN[11];
+    cout << "請輸入十位數";
+    cin >> ISBN;
+    //ISBN = ISBN%10;
+    //Get each digital
+    for (int i=10; i>=1; i--){
+        digitalISBN[i] = ISBN % 10;
+        cout << digitalISBN[i] << "\n";
+        ISBN=ISBN/10;
+    }
+    //Check if the order of digital correct
+    for (int i=1; i<=10; i++){
+        cout << digitalISBN[i] << "\n";
+    }
+
+    //計算檢查碼
+    int checkNumber=0;
+    for (int i=1; i<=9; i++){
+        digitalISBN[i]=digitalISBN[i]*i;
+        checkNumber=checkNumber+digitalISBN[i];
+    }
+    checkNumber = checkNumber % 11;
+    cout << checkNumber << "\n";
+
+    if (checkNumber==digitalISBN[10])
+        cout << "yes";
+    else
+        cout << "no";
+}
